616d237d8b2505f1ac42985e171c9b5c50cc6552
Dongeon Park <dongun8943@gmail.com>
2018-10-16 16:57:08 +0900

    added turbo commit

--- main.c
+++ main.c
@@ -2,9 +2,22 @@
 #include "func1.h"
 #include "func2.h"
 
+void turdo_on()
+{
+	printf("turbo on\n");
+}
+
+void turdo_off()
+{
+	printf("turbo off\n");
+
+}
+
 int main()
 {
+	turbo_on()
 	Foo1();
 	Foo2();
+	turbo_off()
 	return 0;
 }
